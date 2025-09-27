b04100102ea24a1f4cd79c65e75fc34fc2a2a613
msin32 <103911574+msin32@users.noreply.github.com>
2025-09-27 12:00:46 -0400

    Update Main.cpp - use new glbinding initialize

--- Main.cpp
+++ Main.cpp
@@ -100,7 +100,7 @@ int emacs_module_init (emacs_runtime *ert) noexcept {
     emacs_env* env = ert->get_environment (ert);
     try {
         init();
-        glbinding::Binding::initialize(false);
+        glbinding::Binding::initialize(glfwGetProcAddress, false);
     } catch (std::exception& e) {
         reportError(env, e);
     }
